0147ce66093d3c9ef12e133a66c76c95c10680eb
Dang Xuan Canh <canhdx@nhanhoa.com.vn>
2019-01-23 16:57:06 +0700

    Update inject comand bashshell check

--- telebot.py
+++ telebot.py
@@ -15,8 +15,7 @@ list_spec = config['telegram']['chat_id_special'].split(',')
 list_normal = [x.strip(' ') for x in list_normal]
 list_spec = [x.strip(' ') for x in list_spec]
 
-help_msg="""
-/status <service> - Check Status Service 
+help_msg="""/status <service> - Check Status Service 
 /start <service> - Start service 
 /stop <service> - Stop service 
 /restart <service> - Restart service 
@@ -27,10 +26,19 @@ def bash(string):
     """
     Define bash to call bash shell command return 
     """
-    print(string)
     output = subprocess.run(string, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
     return output.stdout
 
+def inject_bash(string):
+    """
+    Remove inject in bash allow only a-z,A-Z,0-9,-_
+    """
+    if (1 <= len(string) <= 25 and re.match('^[0-9a-zA-Z\-\_]+$', string) and 
+        not string.startswith("-") and not string.startswith("_") and
+        not string.endswith("-") and not string.endswith("_")):
+        return False
+    else:
+        return True
 
 def check_status(string):
     """
@@ -50,22 +58,25 @@ dispatcher = updater.dispatcher
 # /status command to all id
 def status(bot, update, args):
     if not args:
-        bot.send_message(chat_id=update.message.chat_id, text="Missing services")
+        message="Missing service"
+    elif inject_bash(args[0]):
+        message="WARNING-Inject detect"
     else:
         message=bash('systemctl status '+args[0])
-        bot.send_message(chat_id=update.message.chat_id, text='```\n'+ message +'```', parse_mode='MARKDOWN')
+    bot.send_message(chat_id=update.message.chat_id, text='```\n'+ message +'```', parse_mode='MARKDOWN')
 
 status_handler = CommandHandler('status', status, pass_args=True)
 dispatcher.add_handler(status_handler)
 
 # /start command to list id
 def start(bot, update, args):
-    stat=check_status(args)
     if str(update.message.chat_id) not in list_normal:
         message="Permision deny"
     elif not args:
         message="Missing service"
-    elif stat == "Running":
+    elif inject_bash(args[0]):
+        message="WARNING-Inject detect"
+    elif check_status(args) == "Running":
         message="Service still running..."
     else:
         message=bash('systemctl start '+args[0])
@@ -76,14 +87,15 @@ dispatcher.add_handler(start_handler)
 
 # /stop command to list id
 def stop(bot, update, args):
-    stat=check_status(args)
     if str(update.message.chat_id) not in list_normal:
         message="Permision deny"
     elif not args:
         message="Missing service"
-    elif stat == "Stopped":
+    elif inject_bash(args[0]):
+        message="WARNING-Inject detect"
+    elif check_status(args) == "Stopped":
         message="Service stopped"
-    else:
+    else: 
         message=bash('systemctl stop '+args[0])
     bot.send_message(chat_id=update.message.chat_id, text='```\n'+ message +'```', parse_mode='MARKDOWN')
 
@@ -96,6 +108,8 @@ def restart(bot, update, args):
         message="Permision deny"
     elif not args:
         message="Missing service"
+    elif inject_bash(args[0]):
+        message="WARNING-Inject detect"
     else:
         message=bash('systemctl restart '+args[0])
     bot.send_message(chat_id=update.message.chat_id, text='```\n'+ message +'```', parse_mode='MARKDOWN')
@@ -121,21 +135,22 @@ dispatcher.add_handler(command_handler)
 
 # /help command
 def help(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=help_msg,parse_mode= 'Markdown')
+    bot.send_message(chat_id=update.message.chat_id, text='```\n' +help_msg+ '```',parse_mode= 'Markdown')
 
 help_handler = CommandHandler('help', help)
 dispatcher.add_handler(help_handler)
 
 # except all text 
 def echo(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Wrong syntax using this command bellow \n"+help_msg)
+    bot.send_message(chat_id=update.message.chat_id, text="```\nWrong syntax using this command bellow \n\n" +help_msg+ '```',parse_mode= 'Markdown')
 
 echo_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(echo_handler)
 
 # except all command not in handler
 def unknown(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Wrong syntax using this command bellow \n"+help_msg)
+    #bot.send_message(chat_id=update.message.chat_id, text="Wrong syntax using this command bellow \n"+help_msg)
+    bot.send_message(chat_id=update.message.chat_id, text="```\nWrong syntax using this command bellow \n\n" +help_msg+ '```',parse_mode= 'Markdown')
 
 unknown_handler = MessageHandler(Filters.command, unknown)
 dispatcher.add_handler(unknown_handler)
